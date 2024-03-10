9626b6daddea517d161d1687be29203469e7c9c3
evoreign <edbert_khovey@yahoo.com>
2024-03-11 00:22:34 +0700

    Add header component and update global CSS

---
+++ FE/image-finder-fe/app/models/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header";
+
+export default function Models() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-20">
+        <Header />
+
+        <h1 className="text-3xl font-bold">Welcome to models page, display all models using tanstack table</h1>
+    </main>
+  );
+}
---
+++ FE/image-finder-fe/components/header.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+
+import logo from "../public/logo-dark.png";
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full p-2 sm:p-4 fixed top-0 left-0 flex justify-between items-center px-2 sm:px-4 py-1 sm:py-2 bg-transparent z-50 text-black">
+      <div className="flex items-center">
+        <Link href="/">
+            <Image src={logo} alt="Logo" width={100} height={100} className="sm:w-125 sm:h-125" />
+        </Link>
+      </div>
+      <div className="flex h-5 items-center space-x-2 sm:space-x-4 text-xs sm:text-sm">
+        <Link href="/" className="text-black hover:text-blue-500">
+            Search
+        </Link>
+        <Link href="/models" className="text-black hover:text-blue-500">
+            Docs
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
