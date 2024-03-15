1322cd860f07a7a8d7e988333d91f7ce524c6b8c
evoreign <edbert_khovey@yahoo.com>
2024-03-15 14:47:02 +0700

    Add new dependencies and implement data table in models page

--- FE/image-finder-fe/app/models/page.tsx
+++ FE/image-finder-fe/app/models/page.tsx
@@ -1,11 +1,25 @@
-import Header from "@/components/header";
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
 
-export default function Models() {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen py-20">
-        <Header />
-
-        <h1 className="text-3xl font-bold">Welcome to models page, display all models using tanstack table</h1>
-    </main>
-  );
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
 }
---
+++ FE/image-finder-fe/app/search/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header";
+
+export default function search() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-20">
+        <Header />
+
+        <h1 className="text-3xl font-bold">Welcome to search page, display all models using tanstack table</h1>
+    </main>
+  );
+}
