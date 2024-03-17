d7df5ab9cc9cdb6cce48b9bc532fb6e5df30a34d
evoreign <edbert_khovey@yahoo.com>
2024-03-17 16:14:53 +0700

    add header etc general styling, next is to fetch api

--- FE/image-finder-fe/app/models/page.tsx
+++ FE/image-finder-fe/app/models/page.tsx
@@ -1,5 +1,8 @@
+"use client"
+import { useState, useEffect } from 'react';
 import { Payment, columns } from "./columns"
 import { DataTable } from "./data-table"
+import Header from "@/components/header";
 
 async function getData(): Promise<Payment[]> {
   // Fetch data from your API here.
@@ -14,12 +17,25 @@ async function getData(): Promise<Payment[]> {
   ]
 }
 
-export default async function DemoPage() {
-  const data = await getData()
+export default function DemoPage() {
+  const [data, setData] = useState<Payment[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getData();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen py-20">
+      <Header />
+      <h1 className="text-3xl font-bold">Welcome to Image Finder</h1>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </main>
   )
 }
