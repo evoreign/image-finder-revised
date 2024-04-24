3751f906b8975081a78a58923cd7edebda66719b
edbert khovey <64061171+evoreign@users.noreply.github.com>
2024-04-24 23:41:09 +0700

    simple result page

--- FE/image-finder-fe/app/image/[UUID]/page.tsx
+++ FE/image-finder-fe/app/image/[UUID]/page.tsx
@@ -2,16 +2,21 @@
 import { useEffect, useState } from 'react';
 import Header from "@/components/header";
 import axios from 'axios'; // Import Axios
+import Image from 'next/image'
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'; // Correct import for useSearchParams
 
 type ParamsType = {
     UUID: string; // Corrected to string as it's typically a UUID
 };
 
+interface ImageData {
+    original_filename: string;
+    secure_url: string;
+}
 
 export default function ImagePage({ params }: { params: ParamsType }) {
-    const [imageData, setImageData] = useState<ImageData | null>(null);
+    const [imageData, setImageData] = useState<ImageData[] | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
     const searchParams = useSearchParams();
@@ -20,7 +25,7 @@ export default function ImagePage({ params }: { params: ParamsType }) {
     useEffect(() => {
         const fetchImageData = async () => {
             try {
-                const response = await axios.get<ImageData>(`http://127.0.0.1:80/imagesearch/${params.UUID}`);
+                const response = await axios.get<ImageData[]>(`http://127.0.0.1:80/imagesearch/${params.UUID}`);
                 setImageData(response.data);
                 setLoading(false);
             } catch (error) {
@@ -40,10 +45,22 @@ export default function ImagePage({ params }: { params: ParamsType }) {
             {error && <div>{error}</div>}
             {imageData && (
                 <div>
-                    <h1>Image for {fileName}</h1>
-                    <pre>{JSON.stringify(imageData, null, 2)}</pre>
+                    <h1>Images for {fileName}</h1>
+                        {imageData.map((image, index) => (
+                            <div className='hover:bg-gray-500'key={index}>
+                                <Image
+                                    src={image.secure_url}
+                                    alt={image.original_filename}
+                                    width={250}
+                                    height={250}
+                                />
+                                <p>{image.original_filename}</p>
+                            </div>
+                        ))}
+
                 </div>
             )}
         </main>
     );
 }
+
