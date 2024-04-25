0119207697fcf8ded7e9bf22e2c55585c5d24ab7
edbert khovey <64061171+evoreign@users.noreply.github.com>
2024-04-25 21:44:31 +0700

    add emergency flush for a collection

---
+++ BE/emergency_flush_collection.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+# Connect to MongoDB Atlas
+client = MongoClient(
+    "mongodb+srv://kopi:kopi@cluster0.1lc1x8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["test"]
+collection = db["attachment-images-sifts"]
+
+# Ask for user confirmation
+print("WARNING: You are about to delete all documents in the collection. This action cannot be undone.")
+confirmation = input("Are you sure you want to continue? (y/n): ")
+
+if confirmation.lower() == 'y':
+    # Delete all documents in the collection
+    result = collection.delete_many({})
+
+    print(f"Deleted {result.deleted_count} documents.")
+else:
+    print("Operation cancelled.")
